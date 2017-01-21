16bce9adaf54868e2a2991414d37332cdbc18eb6
Skanda Narendra Bhargav <skandabhargav@gmail.com>
2017-01-21 14:30:25 -0800

    Placed the drops for waves in a circular fashion around each pedestrian. Will be replaced with the hearts.

--- core/src/com/mygdx/game/Mob.java
+++ core/src/com/mygdx/game/Mob.java
@@ -6,124 +6,128 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Array;
 
 public class Mob implements Collideable {
-	
-	gameScreen game;
-	public static Texture mobImage;
-	public static Texture wave_drop = new Texture(Gdx.files.internal("blue_drop.png"));
 
-    Sprite mobSprite,dropSprite;
+    gameScreen game;
+    public static Texture mobImage;
+    WaveObject wave;
+
+
+    Sprite mobSprite;
     float MobDice;
-	
-	static final int BODY_WIDTH = 50;
-	static final int BODY_HEIGHT = 80;
 
-	boolean controlled;		// Has been waved at
-	boolean waved;			// Has performed a wave
+    static final int BODY_WIDTH = 50;
+    static final int BODY_HEIGHT = 80;
 
-	Node target;			// Node with the position the mob wants to move to
-	Body body;				// Mob body
+    boolean controlled;        // Has been waved at
+    boolean waved;            // Has performed a wave
 
-	// Amount moved by the mob per tick (in pixels)
-	private static final float MOVE_SPEED = 2;
+    Node target;            // Node with the position the mob wants to move to
+    Body body;                // Mob body
 
-	public Mob(gameScreen game, Body body, Node target) {
-		this.game = game;
-		this.body = body;
-		this.target = target;
+    // Amount moved by the mob per tick (in pixels)
+    private static final float MOVE_SPEED = 2;
+
+    public Mob(gameScreen game, Body body, Node target) {
+        this.game = game;
+        this.body = body;
+        this.target = target;
         MobDice = MathUtils.random();
-        if (MobDice > .5f){
+        if (MobDice > .5f) {
             mobImage = new Texture(Gdx.files.internal("badPedestrian.png"));
             mobSprite = new Sprite(mobImage);
         } else {
             mobImage = new Texture(Gdx.files.internal("neutralPedestrian.png"));
             mobSprite = new Sprite(mobImage);
         }
-        mobSprite.setPosition(body.getPosition().x * game.PIXELS_TO_METERS - (BODY_WIDTH/2f), body.getPosition().y * game.PIXELS_TO_METERS - (BODY_HEIGHT/2f));
-		System.out.println(mobSprite.getX());
+        mobSprite.setPosition(body.getPosition().x * game.PIXELS_TO_METERS - (BODY_WIDTH / 2f), body.getPosition().y * game.PIXELS_TO_METERS - (BODY_HEIGHT / 2f));
+        System.out.println(mobSprite.getX());
         mobSprite.setOriginCenter();
         mobSprite.setRotation(0f);
-		wave();
+        wave();
         body.setUserData(this);
-	}
+    }
 
-	// Method that gets called whenever the game is "updating"
-	public void tick() {
+    // Method that gets called whenever the game is "updating"
+    public void tick() {
 
-		moveTowardTarget();
+        moveTowardTarget();
 
-	}
+    }
 
-	public void render(Batch batch) {
+    public void render(Batch batch) {
         batch.draw(mobSprite, mobSprite.getX(), mobSprite.getY(), mobSprite.getOriginX(), mobSprite.getOriginY(), mobSprite.getWidth(), mobSprite.getHeight(), mobSprite.getScaleX(), mobSprite.getScaleY(), mobSprite.getRotation());
-		batch.draw(dropSprite, dropSprite.getX(), dropSprite.getY(), dropSprite.getOriginX(), dropSprite.getOriginY(), dropSprite.getWidth(), dropSprite.getHeight(), dropSprite.getScaleX(), dropSprite.getScaleY(), dropSprite.getRotation());
-	}
+        //batch.draw(wave.dropSprites.first(), wave.dropSprites.first().getX(), wave.dropSprites.first().getY(), wave.dropSprites.first().getOriginX(), wave.dropSprites.first().getOriginY(), wave.dropSprites.first().getWidth(), wave.dropSprites.first().getHeight(), wave.dropSprites.first().getScaleX(), wave.dropSprites.first().getScaleY(), wave.dropSprites.first().getRotation());
+        wave.drawWave(batch);
+    }
 
-	/**
-	 * Code to generate wave
-	 * Place the projectiles around the person object in a circular fashion
-	 * Each projectile will be a 2x2 blue rectangle travelling for some 'd' distance
-	 * Number of projectiles = 360/18 = 20
-	 * Need to calculate each projectiles center location (x/y) around the particular person object
-	 * To be decided: 1. Velocity of the projectile
-	 * 2. TTL = Time to live. The projectile will expire after 's' seconds
-	 */
-	public void wave() {
-		dropSprite = new Sprite(wave_drop);
-		dropSprite.setPosition(mobSprite.getX()+25+48,mobSprite.getY()+40+48);
-		dropSprite.setOriginCenter();
-		
-	}
+    /**
+     * Code to generate wave
+     * Place the projectiles around the person object in a circular fashion
+     * Each projectile will be a 2x2 blue rectangle travelling for some 'd' distance
+     * Number of projectiles = 360/18 = 20
+     * Need to calculate each projectiles center location (x/y) around the particular person object
+     * To be decided: 1. Velocity of the projectile
+     * 2. TTL = Time to live. The projectile will expire after 's' seconds
+     */
+    public void wave() {
+        wave = new WaveObject();
+        wave.positionDrops(mobSprite.getX() + 25, mobSprite.getY() + 40);
+    }
 
-	public Node getTarget() {
-		return target;
-	}
+    public Node getTarget() {
+        return target;
+    }
 
-	public void setTarget(Node newTarget) {
-		target = newTarget;
-	}
+    public void setTarget(Node newTarget) {
+        target = newTarget;
+    }
 
-	// returns true when the mob is at the target node
-	private boolean atTarget() {
-		return (body.getPosition().x == target.getYPos()) && (body.getPosition().y == target.getYPos()); // Update with collision code
-	}
+    // returns true when the mob is at the target node
+    private boolean atTarget() {
+        return (body.getPosition().x == target.getYPos()) && (body.getPosition().y == target.getYPos()); // Update with collision code
+    }
 
-	// Moves the target one "MOVE_SPEED" towards the target Node
-	public void moveTowardTarget() {
-		// Note: this code is based on the assumption that there is a straight line between
-		// the mob and its target node. This does come with the advantage that it allows for
-		// diagonal movement paths
+    // Moves the target one "MOVE_SPEED" towards the target Node
+    public void moveTowardTarget() {
+        // Note: this code is based on the assumption that there is a straight line between
+        // the mob and its target node. This does come with the advantage that it allows for
+        // diagonal movement paths
 
-		float dX = MOVE_SPEED * (float) Math.cos(target.getXPos() / getXPos());
-		float dY = MOVE_SPEED * (float) Math.sin(target.getYPos() / getYPos());
+        float dX = MOVE_SPEED * (float) Math.cos(target.getXPos() / getXPos());
+        float dY = MOVE_SPEED * (float) Math.sin(target.getYPos() / getYPos());
 
-		setPos(getXPos() + dX, getYPos() + dY);
-	}
+        setPos(getXPos() + dX, getYPos() + dY);
+    }
 
-	// Moving the position access calls into a helper to increase code readability
-	public float getXPos() {
-		return body.getPosition().x;
-	}
+    // Moving the position access calls into a helper to increase code readability
+    public float getXPos() {
+        return body.getPosition().x;
+    }
 
-	public float getYPos() {
-		return body.getPosition().y;
-	}
+    public float getYPos() {
+        return body.getPosition().y;
+    }
 
-	public void setPos(float newXPos, float newYPos) {
-		body.getPosition().set(newXPos, newYPos);
-	}
+    public void setPos(float newXPos, float newYPos) {
+        body.getPosition().set(newXPos, newYPos);
+    }
 
-	public void dispose(){
-		mobImage.dispose();
-	}
+    public void dispose() {
+        mobImage.dispose();
+    }
 
-	public void onCollide(Object entity) {
-		}
-	@Override
-	public void onCollide(Collideable object) {
-		if (object instanceof WaveObject) {
-			controlled = true;
-		}	
-	}
+    public void onCollide(Object entity) {
+    }
+
+    @Override
+    public void onCollide(Collideable object) {
+        if (object instanceof WaveObject) {
+            controlled = true;
+        }
+    }
+
+
 }
--- core/src/com/mygdx/game/WaveObject.java
+++ core/src/com/mygdx/game/WaveObject.java
@@ -1,6 +1,18 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.Array;
+
+import static java.lang.Math.*;
+
+
 public class WaveObject implements Collideable {
+	Array<Sprite> dropSprites = new Array<Sprite>(20);
+	Texture wave_drop = new Texture(Gdx.files.internal("blue_drop.png"));
+
 
 	@Override
 	public void onCollide(Collideable object) {
@@ -8,4 +20,26 @@ public class WaveObject implements Collideable {
 		
 	}
 
+	/**
+	 * Methond to get a circle around the object
+	 * The drops will be placed at even distance on this circle
+	 */
+	protected void positionDrops(float centerX, float centerY) {
+		for (int i = 0; i < 360; i = i + 18) {
+			Sprite dropSprite = new Sprite(wave_drop);
+			int r = 48;
+			double x, y;
+			x = centerX + r * cos(i);
+			y = centerY + r * sin(i);
+			dropSprite.setPosition(((float) x), (float) y);
+			dropSprites.add(dropSprite);
+		}
+	}
+
+	protected void drawWave(Batch batch) {
+		for (Sprite thisSprite : dropSprites
+			 ) {
+			batch.draw(thisSprite, thisSprite.getX(), thisSprite.getY(), thisSprite.getOriginX(), thisSprite.getOriginY(), thisSprite.getWidth(), thisSprite.getHeight(), thisSprite.getScaleX(), thisSprite.getScaleY(), thisSprite.getRotation());
+		}
+	}
 }
