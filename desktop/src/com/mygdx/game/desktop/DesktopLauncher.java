ef5d67a4c2b13e8adbb80af6d3e657595eeb4e2f
Ben Thomas <bthomas2622@gmail.com>
2017-01-20 20:10:56 -0800

    launcher files

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.TeamWave;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Team Wave";
+        config.width = 1920;
+        config.height = 1080;
 		new LwjglApplication(new TeamWave(), config);
 	}
 }
