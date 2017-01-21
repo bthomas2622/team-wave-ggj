a4fca4b3109e427fa64c4319ae4a76d00edc761e
Ben Thomas <bthomas2622@gmail.com>
2017-01-20 22:39:18 -0800

    drawing mobs

--- core/src/com/mygdx/game/Mob.java
+++ core/src/com/mygdx/game/Mob.java
@@ -1,11 +1,18 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.Body;
 
 public class Mob {
 	
 	gameScreen game;
+	Texture mobImage;
+    Sprite mobSprite;
+    float MobDice;
 	
 	static final int BODY_WIDTH = 80;
 	static final int BODY_HEIGHT = 50;
@@ -20,6 +27,18 @@ public class Mob {
 		this.game = game;
 		this.body = body;
 		this.target = target;
+        MobDice = MathUtils.random();
+        if (MobDice > .5f){
+            mobImage = new Texture(Gdx.files.internal("badPedestrian.png"));
+            mobSprite = new Sprite(mobImage);
+        } else {
+            mobImage = new Texture(Gdx.files.internal("neutralPedestrian.png"));
+            mobSprite = new Sprite(mobImage);
+        }
+        mobSprite.setPosition(body.getPosition().x, body.getPosition().y);
+        mobSprite.setOriginCenter();
+        mobSprite.setRotation(0f);
+
 	}
 	
 	public void tick() {
@@ -27,7 +46,7 @@ public class Mob {
 	}
 	
 	public void render(Batch batch) {
-		
+        batch.draw(mobSprite, mobSprite.getX(), mobSprite.getY(), mobSprite.getOriginX(), mobSprite.getOriginY(), mobSprite.getWidth(), mobSprite.getHeight(), mobSprite.getScaleX(), mobSprite.getScaleY(), mobSprite.getRotation());
 	}
 	
 	public void wave() {
@@ -41,7 +60,9 @@ public class Mob {
 	public void moveTowardTarget() {
 		
 	}
-	
-	
+
+	public void dispose(){
+		mobImage.dispose();
+	}
 	
 }
