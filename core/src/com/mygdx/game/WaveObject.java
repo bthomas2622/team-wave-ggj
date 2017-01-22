2d60a398e607b0f16b6efa9b35a080514607dc7c
Dominic Mortlock <domi_nicky@hotmail.com>
2017-01-21 16:42:02 -0800

    hearts follow bodies

--- core/src/com/mygdx/game/WaveObject.java
+++ core/src/com/mygdx/game/WaveObject.java
@@ -13,10 +13,10 @@ import static java.lang.Math.sin;
 
 
 public class WaveObject implements Collideable {
-	Array<Sprite> dropSprites = new Array<Sprite>(20);
+	Sprite dropSprite;
 	Texture wave_drop = new Texture(Gdx.files.internal("waveProjectile.png"));
 	gameScreen gameScreen;
-	Body body;
+	Array<Body> bodies = new Array<Body>(20);
 
 	public WaveObject(gameScreen gs) {
 		gameScreen = gs;
@@ -34,15 +34,13 @@ public class WaveObject implements Collideable {
 	 * The drops will be placed at even distance on this circle
 	 */
 	protected void positionDrops(float centerX, float centerY) {
+		dropSprite = new Sprite(wave_drop);
 		for (int i = 0; i < 360; i = i + 18) {
-			Sprite dropSprite = new Sprite(wave_drop);
 			int r = 48;
 			double x, y;
 			x = centerX + r * cos(i);
 			y = centerY + r * sin(i);
-			dropSprite.setPosition(((float) x), (float) y);
-			dropSprites.add(dropSprite);
-			body = gameScreen.map.createBody(((int) x) + 8, (int) y + 8, 16, 16);
+			Body body = gameScreen.map.createBody(((int) x) + 8, (int) y + 8, 16, 16);
 
 
 			float velocity = (float) 0.5; // Your desired velocity of the car.
@@ -52,18 +50,19 @@ public class WaveObject implements Collideable {
 			float velY = MathUtils.sin(angle) * velocity; // Y-component.
 			body.setLinearVelocity(velX, velY);
 			body.setUserData(this);
+			bodies.add(body);
 		}
 	}
 
-
 	protected void drawWave(Batch batch) {
-		for (Sprite thisSprite : dropSprites
-			 ) {
-			batch.draw(thisSprite, thisSprite.getX(), thisSprite.getY(), thisSprite.getOriginX(), thisSprite.getOriginY(), thisSprite.getWidth(), thisSprite.getHeight(), thisSprite.getScaleX(), thisSprite.getScaleY(), thisSprite.getRotation());
+		for (Body body: bodies) {
+			System.out.println("drawing");
+			dropSprite.setPosition(((float) body.getPosition().x * gameScreen.PIXELS_TO_METERS) - (16 / 2f), (float) body.getPosition().y * gameScreen.PIXELS_TO_METERS - (16 / 2f));
+			batch.draw(dropSprite, dropSprite.getX(), dropSprite.getY(), dropSprite.getOriginX(), dropSprite.getOriginY(), dropSprite.getWidth(), dropSprite.getHeight(), dropSprite.getScaleX(), dropSprite.getScaleY(), dropSprite.getRotation());
 		}
 	}
 
 	public Body getBody() {
-		return body;
+		return bodies.first();
 	}
 }
